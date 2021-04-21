29ed68ecd01b67a2e83d86298cabd75f33cad55b
Duc Tran Minh <ductm@devblock.net>
2021-04-21 14:45:20 +0700

    feat: create accordion component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron';
 import { FooterContainer } from './containers/footer';
+import { FaqsContainer } from './containers/faqs';
 
 function App() {
   return (
     <div className="App">
       <JumbotronContainer />
+      <FaqsContainer />
       <FooterContainer />
     </div>
   );
