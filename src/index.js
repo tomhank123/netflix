0ef50bdd38aba8698842fa6e0f8b05a2384dd263
Duc Tran Minh <ductm@devblock.net>
2021-04-21 13:49:01 +0700

    feat: create jumbotron component, global styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { GlobalStyles } from './global-styles';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 serviceWorkerRegistration.unregister();
 
