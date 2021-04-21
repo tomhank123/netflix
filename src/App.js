1cdfc47fb8effe7500b670a95430f0985cd7fc55
Duc Tran Minh <ductm@devblock.net>
2021-04-21 14:28:15 +0700

    feat: create footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { JumbotronContainer } from './containers/jumbotron';
+import { FooterContainer } from './containers/footer';
 
 function App() {
   return (
     <div className="App">
       <JumbotronContainer />
+      <FooterContainer />
     </div>
   );
 }
