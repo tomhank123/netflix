a3f19f74ce45dd38a0a85d0d998d9991725dff8c
Duc Tran Minh <ductm@devblock.net>
2021-04-21 17:57:01 +0700

    feat: create browser page, profile page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ import { useAuthListener } from './hooks';
 function App() {
   const { user } = useAuthListener();
 
-  console.log('user', user);
-
   return (
     <Router>
       <Switch>
--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useContent } from '../hooks';
+import { selectionFilter } from '../utils';
+import { BrowseContainer } from '../containers/browse';
 
 export default function Browse() {
+  const { series } = useContent('series');
+  const { films } = useContent('films');
+  const slides = selectionFilter({ series, films });
+
+  console.log('slides', slides);
+
   return (
-    <div>Browse!</div>
+    <BrowseContainer slides={slides} />
   )
 }
