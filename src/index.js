5b8e10f59ce7043bdf05d941c9d708af7c46dc70
Duc Tran Minh <ductm@devblock.net>
2021-04-21 15:04:43 +0700

    create opt form and import normalize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import { GlobalStyles } from './global-styles';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
