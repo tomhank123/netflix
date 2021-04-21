24f9723e2e8400ae7390c73660ba79e9aa84998e
Duc Tran Minh <ductm@devblock.net>
2021-04-21 16:16:54 +0700

    feat: create sign in page

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Accordion } from './accordion';
 export { default as Feature } from './feature';
+export { default as Form } from './form';
 export { default as Footer } from './footer';
 export { default as Header } from './header';
 export { default as Jumbotron } from './jumbotron';
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from '../components';
+import { FooterContainer } from '../containers/footer';
+import { HeaderContainer } from '../containers/header';
 
 export default function SignIn() {
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error] = useState('');
+
+  const isInvalid = password === '' || emailAddress === '';
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+
+    // firebase work here
+  };
+
   return (
-    <div>SignIn!</div>
-  )
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit
+              disabled={isInvalid}
+              type="submit"
+              data-testid="sign-in"
+            >
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
